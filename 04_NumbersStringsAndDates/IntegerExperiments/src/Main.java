ba29de5a9946433fed3b8836f481648f9fda4474
Othernik <Folko85@yandex.ru>
2020-02-28 00:11:45 +0300

    Homework 4.2 MaxValue and MinValue

--- 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
+++ 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
@@ -7,6 +7,19 @@ public class Main
         System.out.println("Возьмём число: " + container.count);
         System.out.println("Сумма его цифр: " + sumDigits(container.count));
         System.out.println("Посчитаем вторым методом: " + otherSumDigits(container.count));
+        // Домашнее задание 4.2
+        System.out.println("Минимальное значение byte " + Byte.MIN_VALUE);
+        System.out.println("Максимальное значение byte " + Byte.MAX_VALUE);
+        System.out.println("Минимальное значение short " + Short.MIN_VALUE);
+        System.out.println("Максимальное значение short " + Short.MAX_VALUE);
+        System.out.println("Минимальное значение int " + Integer.MIN_VALUE);
+        System.out.println("Максимальное значение int " + Integer.MAX_VALUE);
+        System.out.println("Минимальное значение long " + Long.MIN_VALUE);
+        System.out.println("Максимальное значение long " + Long.MAX_VALUE);
+        System.out.println("Минимальное значение float -" + Float.MAX_VALUE);  // в float и double - минус хранится отдельно
+        System.out.println("Максимальное значение float " + Float.MAX_VALUE);
+        System.out.println("Минимальное значение double -" + Double.MAX_VALUE);  //MIN_VALUE там - минимальное положительное значение
+        System.out.println("Максимальное значение double " + Double.MAX_VALUE);
     }
 
     public static Integer sumDigits(Integer number)  // сделал метод статическим, чтобы проверить
