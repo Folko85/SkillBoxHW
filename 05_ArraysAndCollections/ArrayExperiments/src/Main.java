f61f1b1418bebc889a5a83d284d9e24e6c1a3d00
Othernik <Folko85@yandex.ru>
2020-03-21 16:30:37 +0300

    Homework 5.1 with Stream API

--- 05_ArraysAndCollections/ArrayExperiments/src/Main.java
+++ 05_ArraysAndCollections/ArrayExperiments/src/Main.java
@@ -1,5 +1,8 @@
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.Arrays;
+import java.util.OptionalDouble;
+import java.util.stream.DoubleStream;
 
 public class Main {
     public static void main(String[] args) {
@@ -22,19 +25,15 @@ public class Main {
         // средняя температура по больнице
         int minValue = 32;
         int maxValue = 40;
-        double sumOfTemperatures = 0.0;
         int healthyPatientsCount = 0; // счётчик здоровых пациентов
+        double minHealthyTemperature = 36.2;
+        double maxHealthyTemperature = 36.9;
         double patients[] = new double[30];
         for (int k = 0; k < patients.length; k++) {
             patients[k] = randomValue(minValue, maxValue);
-            sumOfTemperatures += patients[k];   // сразу при создании можно считать и сумму элементов, которую потом поделим на их количество
-            if (patients[k] >= 36.2 && patients[k] <= 36.9) {
-                healthyPatientsCount++;
-            }
         }
-        double averageTemperature = sumOfTemperatures / patients.length;
-        System.out.format("Средняя температура по больнице: %.1f%n", averageTemperature);
-        System.out.println("Здоровых пациентов: " + healthyPatientsCount);
+        System.out.format("Средняя температура по больнице: %.1f%n", calculateAverage(patients));
+        System.out.println("Здоровых пациентов: " + countHealthPatients(patients, minHealthyTemperature, maxHealthyTemperature));
 
         String[][] crossing = new String[7][7];                     // крестик рисуем двумерным массивом
         for (int row = 0; row < crossing.length; row++) {
@@ -52,11 +51,24 @@ public class Main {
 
     }
 
-    public static double randomValue(int min, int max)  //оформим методом
+    public static double randomValue(int min, int max)  //оформим методом получение случайных значений
     {
         max -= min;   // диапазон Math.random считается от нуля, вычтем min, потом прибавим
         double result = (Math.random() * max) + min;
         double around = new BigDecimal(result).setScale(1, RoundingMode.HALF_UP).doubleValue(); // округляем до десятых по обзщепринятым правилам округления
         return around;
+    }
+
+    public static double calculateAverage(double[] array)  //вычисление средней температуры
+    {
+        DoubleStream stream = Arrays.stream(array);
+        return stream.average().getAsDouble();
+
+    }
+
+    public static int countHealthPatients(double[] array, double minTemperature, double maxTemperature)  //подсчёт количества здоровых пациентов
+    {
+        DoubleStream stream = Arrays.stream(array);
+        return (int) stream.filter(a -> a >= minTemperature && a <= maxTemperature).count();
     }
 }
