da12549db8941053ee6b29f046cd1c01514c0ba4
Othernik <Folko85@yandex.ru>
2020-06-03 04:26:03 +0300

    Homework 9.1 Light Remake

--- 09_FilesAndNetwork/FolderSize/src/Main.java
+++ 09_FilesAndNetwork/FolderSize/src/Main.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
@@ -9,15 +10,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // мы не ищем лёгких путей. Находить размер папки будем с помощью интерфейса FileVisitor
-        while (true) {
-            System.out.println("Введите имя папки: ");
-            Scanner scanner = new Scanner(System.in);
-            String input = scanner.nextLine();
-            Path path = Paths.get(input);
+        String input;      //
+        Path path;
 
+        while (true) {
+            if (args.length > 1) {
+                System.out.println("Программа может иметь только один аргумент - имя папки.");
+                break;        // если кто-то решил ввести несколько аргуметов
+            } else if (args.length == 0) {
+                input = inputPathName();  // если программа вызвана без параметров, то она сама предложит ввести имя файла
+            } else {
+                input = args[0]; // в противном случае имя папки берётся из единственного параметра
+            }
+            try {
+                path = Paths.get(input);
+            } catch (InvalidPathException ex) {
+                System.out.println("Введено некорректное имя файла: " + input);
+                System.out.println("Возникла ошибка: " + ex);
+                break;
+            }
             if (!Files.exists(path) || !Files.isDirectory(path)) {   // избавляемся от неверного ввода или от ввода имени файла
-                System.out.println("Вы ввели невозможное имя.");     // мы ведь считаем размер папки.
-                continue;
+                System.out.println("Вы ввели невозможное имя папки.");     // мы ведь считаем размер папки.
+                break;                                              // если имя папки введено неверно, программа будет прекращать выполнение
             }
             FolderSizeVisitor visitor = new FolderSizeVisitor(path);
             try {
@@ -31,14 +45,22 @@ public class Main {
             if (visitor.getExceptionCounter() > 0) {
                 System.out.println("В ходе подсчёта не удалось получить доступ к некоторым файлам и папкам. Количество непрочитанных элементов: " + visitor.getExceptionCounter());
             }
-
+            break;
         }
+    }
+
+
+    public static String inputPathName() {            // вынесем это в отдельный метод и будем вызывать, если пользователь вызвал программу без параметров
+        System.out.println("Введите имя папки: ");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        return input;
     }
 
     public static String sizeDef(long sizeByte)  // вынесем отпределение размерности в отдельный метод
     {
         BigDecimal sizeKiloByte = new BigDecimal(sizeByte / 1024).setScale(2, RoundingMode.HALF_UP);
-        BigDecimal sizeMegaByte = sizeKiloByte.divide(new BigDecimal(1024)).setScale(2,RoundingMode.HALF_UP);
+        BigDecimal sizeMegaByte = sizeKiloByte.divide(new BigDecimal(1024)).setScale(2, RoundingMode.HALF_UP);
         if (sizeByte < 1024) {
             return sizeByte + " Б";
         } else if (sizeKiloByte.compareTo(new BigDecimal(1024)) < 0) {
@@ -46,6 +68,6 @@ public class Main {
         } else if (sizeMegaByte.compareTo(new BigDecimal(1024)) < 0) {
             return sizeMegaByte + " Мб";
         }
-        return sizeMegaByte.divide(new BigDecimal(1024)).setScale(2,RoundingMode.HALF_UP) + " Гб";
+        return sizeMegaByte.divide(new BigDecimal(1024)).setScale(2, RoundingMode.HALF_UP) + " Гб";
     }
 }
