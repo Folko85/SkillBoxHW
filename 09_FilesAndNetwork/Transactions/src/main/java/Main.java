cb75449c5ba06e2917ce454ccbdc2b437a4e5c07
Othernik <Folko85@yandex.ru>
2020-06-23 20:22:23 +0300

    Homework 9.3 SomeFixes and Improves

--- 09_FilesAndNetwork/Transactions/src/main/java/Main.java
+++ 09_FilesAndNetwork/Transactions/src/main/java/Main.java
@@ -1,3 +1,8 @@
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
+
 import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -12,12 +17,17 @@ import java.util.stream.Collectors;
 
 public class Main {
 
+    private static final Logger logger = LogManager.getLogger(Main.class);
     private static String csvFile = "./09_FilesAndNetwork/Transactions/data/movementList.csv";// адрес оставляю таким, так как проект у меня модуль общего проекта ДЗ
     private static DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd.MM.yy");
     private static Pattern subject = Pattern.compile("([\\d\\S]+)+([\\s\\d]+)+([/\\\\][\\w\\s\\\\/>]+)+\\s{10}([\\w .]+)+");
+    private static final int DATE_INDEX = 3;
+    private static final int DESCRIPTION_INDEX = 5;
+    private static final int INCOMING_INDEX = 6;
+    private static final int OUTGOING_INDEX = 7;
 
     public static void main(String[] args) {
-
+        Configurator.setLevel(logger.getName(), Level.DEBUG);  //Теперь логи будут писаться, несмотря на более высокий уровень логирования в настройках
         ArrayList<Transaction> operations = loadFromFile(csvFile);
         BigDecimal sumInput = operations.stream().map(Transaction::getIncomingMoney).reduce(BigDecimal.ZERO, BigDecimal::add);
         System.out.println("Сумма доходов " + sumInput);
@@ -37,30 +47,37 @@ public class Main {
             lines.remove(0);           // убираем заголовки таблицы
             for (String line : lines) {                              // каждую строку
                 String[] fragments = line.split(",");            // мы делим запятыми и записываем в массив
-                ArrayList<String> columnList = new ArrayList<>(); // а это список строк, на основании которого мы будем создавать список транзакций
-                for (String fragment : fragments) {             // каждый фрагмент массива мы обрабатываем, чтобы учесть запятые внутри кавычек
-                    if (IsColumnPart(fragment)) {                              //Если колонка заканчиваеться на кавычки
-                        String lastText = columnList.get(columnList.size() - 1);     // то получаем предыдущий фрагмент
-                        columnList.set(columnList.size() - 1, lastText + "," + fragment);// и прибавляем к нему текущий фрагмент
-                    } else {
-                        columnList.add(fragment);           // иначе просто добавляем фрагмент в список
-                    }
-                }
-                LocalDate date = LocalDate.parse(columnList.get(3), dateFormat);  // на самом деле дату нам парсить не нужно, просто для тренировки
-                String description = normalizeDescription(columnList.get(5));
-                BigDecimal incomeMoney = new BigDecimal(columnList.get(6).replaceAll(",", ".").replaceAll("\"", ""));
-                BigDecimal outgoingMoney = new BigDecimal(columnList.get(7).replaceAll(",", ".").replaceAll("\"", ""));
+                ArrayList<String> columnList = fragmentsToList(fragments); // а это список строк, на основании которого мы будем создавать список транзакций
+                LocalDate date = LocalDate.parse(columnList.get(DATE_INDEX), dateFormat);  // на самом деле дату нам парсить не нужно, просто для тренировки
+                String description = normalizeDescription(columnList.get(DESCRIPTION_INDEX));
+                BigDecimal incomeMoney = new BigDecimal(columnList.get(INCOMING_INDEX).replaceAll(",", ".").replaceAll("\"", ""));
+                BigDecimal outgoingMoney = new BigDecimal(columnList.get(OUTGOING_INDEX).replaceAll(",", ".").replaceAll("\"", ""));
                 Transaction transaction = new Transaction(date, description, incomeMoney, outgoingMoney);
+                logger.debug("Для теста нам хватит и одной строчки. " + transaction);
+                Configurator.setLevel(logger.getName(), Level.INFO);  // лог отключён, так как мы повысили уровень логирования. Ибо нефиг писать логи по 120 строчек
                 result.add(transaction);
             }
         } catch (Exception ex) {
-            ex.printStackTrace();
+            logger.error("Возникла ошибка" + ex);
         }
         return result;
     }
 
+    private static ArrayList<String> fragmentsToList(String[] fragments) {
+        ArrayList<String> resultList = new ArrayList<>();
+        for (String fragment : fragments) {             // каждый фрагмент массива мы обрабатываем, чтобы учесть запятые внутри кавычек
+            if (isColumnPart(fragment)) {                              //Если колонка заканчиваеться на кавычки
+                String lastText = resultList.get(resultList.size() - 1);     // то получаем предыдущий фрагмент
+                resultList.set(resultList.size() - 1, lastText + "," + fragment);// и прибавляем к нему текущий фрагмент
+            } else {
+                resultList.add(fragment);           // иначе просто добавляем фрагмент в список
+            }
+        }
+        return resultList;
+    }
+
     //Проверка является ли колонка частью предыдущей колонки, грубовато, но для данного случая достаточно
-    private static boolean IsColumnPart(String text) {    // Вариант с двумя запятыми внутри кавычек не рассматриваем, так как в нашем файле такого нет
+    private static boolean isColumnPart(String text) {    // Вариант с двумя запятыми внутри кавычек не рассматриваем, так как в нашем файле такого нет
         String trimText = text.trim();
         //Если в тексте одна ковычка и текст на нее заканчиваеться значит это часть предыдущей колонки
         return trimText.indexOf("\"") == trimText.lastIndexOf("\"") && trimText.endsWith("\"");
