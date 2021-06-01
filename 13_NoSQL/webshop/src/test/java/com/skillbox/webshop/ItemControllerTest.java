64f011050a9e35640882c8dccb989c95d53eb928
Gutorov <Folko85@yandex.ru>
2021-06-02 01:10:54 +0300

    Похоже, я породил монстра. Статический вложенный класс с настройками. При этом работа этого класса всё ещё под сомнением.

--- 13_NoSQL/webshop/src/test/java/com/skillbox/webshop/ItemControllerTest.java
+++ 13_NoSQL/webshop/src/test/java/com/skillbox/webshop/ItemControllerTest.java
@@ -1,12 +1,20 @@
 package com.skillbox.webshop;
 
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoDriverInformation;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.internal.MongoClientImpl;
+import org.junit.jupiter.api.*;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
@@ -16,7 +24,8 @@ import org.testcontainers.utility.DockerImageName;
 
 
 @Testcontainers
-@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@SpringBootTest(classes = {WebshopApplication.class, ItemControllerTest.SpringMongoConfig.class })
+@EnableAutoConfiguration(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 class ItemControllerTest extends ApplicationTest {
 
     @Container
@@ -39,17 +48,53 @@ class ItemControllerTest extends ApplicationTest {
         }
     }
 
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @AfterEach
+    void tearDown() {
+//        itemRepository.deleteAll();
+    }
+
     @Test
     public void testGetAllItems() throws Exception {
-        System.out.println("Тест пройден");
-        assert true;
 //        Item item = new Item().setName("Имя").setCategory("Категория").setPrice(100D);
 //        itemRepository.save(item);
 //        mockMvc.perform(MockMvcRequestBuilders
 //                .get("/item/list/all")
 //                .accept(MediaType.APPLICATION_JSON))
 //                .andExpect(MockMvcResultMatchers.status().isOk());
-////                .andExpect(MockMvcResultMatchers.content().json(mapper.writeValueAsString(Arrays.asList(item))));
+//                .andExpect(MockMvcResultMatchers.content().json(mapper.writeValueAsString(Arrays.asList(item))));
+    }
+
+
+    @Configuration
+    public static class SpringMongoConfig {
+
+        public String getDatabaseName() {
+            return "test";
+        }
+
+        @Bean
+        public MongoClient mongoClient() {
+            MongoClientSettings settings = MongoClientSettings.builder().applyConnectionString(new ConnectionString(mongoDBContainer.getReplicaSetUrl())).build();
+            MongoDriverInformation information = MongoDriverInformation.builder().build();
+            MongoClient client = new MongoClientImpl(settings, information);
+            return client;
+        }
+
+        @Bean
+        public SimpleMongoClientDatabaseFactory mongoDbFactory() throws Exception {
+            return new SimpleMongoClientDatabaseFactory(mongoClient(), getDatabaseName());
+        }
+
+        @Bean
+        public MongoTemplate mongoTemplate() throws Exception {
+            MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory());
+            return mongoTemplate;
+        }
     }
 
 }
