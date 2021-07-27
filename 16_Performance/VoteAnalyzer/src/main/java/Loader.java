76053b14fd454bd46e464b6c8cce9738ae8337e4
GutorovMikhail <Folko85@yandex.ru>
2021-07-27 22:47:11 +0300

    Homework 19.3 Database writing again

--- 16_Performance/VoteAnalyzer/src/main/java/Loader.java
+++ 16_Performance/VoteAnalyzer/src/main/java/Loader.java
@@ -35,11 +35,18 @@ public class Loader {
 //        System.out.println("SAX-парсер занимает памяти: " + usage/ 1_000_000 + "Mb\n");
 
         SuperHandler superHandler = new SuperHandler();
+        long parseAndWritingTime = System.currentTimeMillis();
         parser.parse(new File(fileName), superHandler);
+        parseAndWritingTime = System.currentTimeMillis() - parseAndWritingTime;
+        long readTime = System.currentTimeMillis();
         superHandler.duplicatedVoters();
-
+        readTime = System.currentTimeMillis() - readTime;
         time = System.currentTimeMillis() - time;
         LocalTime result = Instant.ofEpochMilli(time).atZone(ZoneId.systemDefault()).toLocalTime();
+        LocalTime resultWrite = Instant.ofEpochMilli(parseAndWritingTime).atZone(ZoneId.systemDefault()).toLocalTime();
+        LocalTime resultRead = Instant.ofEpochMilli(readTime).atZone(ZoneId.systemDefault()).toLocalTime();
         System.out.println("И файл 1,5Гб записывается в базу данных за : " + result.getMinute()  + " минут и " + result.getSecond() + " секунд\n");
+        System.out.println("При этом время парсинга и записи в базу составляет : " + resultWrite.getMinute()  + " минут и " + resultWrite.getSecond() + " секунд\n");
+        System.out.println("Время чтения из базы составляет : " + resultRead.getMinute()  + " минут и " + resultRead.getSecond() + " секунд\n");
     }
 }
